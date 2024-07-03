1d743c4a917411b18a8038cd7c41cc9a1ea06cce
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-03 15:53:20 +0000

    Add functionality to add new projects

--- src/views/ui.js
+++ src/views/ui.js
@@ -1,5 +1,6 @@
 import Storage from '../utils/storage'
 import Task from '../models/task'
+import Project from '../models/project'
 
 export default class UI {
   constructor () {
@@ -7,6 +8,7 @@ export default class UI {
     this.sidebarToggler = document.querySelector('.sidebar-toggler')
     this.mainContents = document.querySelector('.main-contents')
     this.addTaskForm = document.querySelector('.add-task-form')
+    this.addProjectForm = document.querySelector('.add-project-form')
 
     this.sidebarToggler.addEventListener('click', () => {
       this.sidebar.classList.toggle('sidebar-hide')
@@ -39,7 +41,22 @@ export default class UI {
       } else {
         event.stopPropagation()
         this.addTaskForm.classList.add('was-validated')
-        console.log('here')
+      }
+    })
+
+    this.addProjectForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+
+      if (this.addProjectForm.checkVisibility()) {
+        const name = this.addProjectForm.querySelector('#name').value
+        const newProject = new Project(name)
+        Storage.addProject(newProject)
+
+        this.addProjectForm.reset()
+        this.addProjectForm.classList.remove('was-validated')
+      } else {
+        event.stopPropagation()
+        this.addProjectForm.classList.add('was-validated')
       }
     })
   }
