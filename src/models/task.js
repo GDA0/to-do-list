c475ae41d3ad5553325c0a0779dec377a36cb0b4
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-05 15:31:58 +0000

    Refactor task class

--- src/models/task.js
+++ src/models/task.js
@@ -1,10 +1,20 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export default class Task {
-  constructor (name, description, dueDate, priority, parentProjectId) {
+  constructor (
+    name,
+    description,
+    dueDate,
+    priority,
+    parentProjectId,
+    id = uuidv4()
+  ) {
     this.name = name
     this.description = description
     this.dueDate = dueDate
     this.priority = priority
     this.completed = false
+    this.id = id
     this.parentProjectId = parentProjectId
   }
 
@@ -12,7 +22,20 @@ export default class Task {
     this.completed = !this.completed
   }
 
-  editTask (editedTask) {
-    Object.assign(this, editedTask)
+  update (updatedTask) {
+    Object.assign(this, updatedTask)
+  }
+
+  static fromJSON (json) {
+    const task = new Task(
+      json.name,
+      json.description,
+      json.dueDate,
+      json.priority,
+      json.parentProjectId,
+      json.id
+    )
+    task.completed = json.completed
+    return task
   }
 }
